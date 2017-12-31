a3cad375923f3ac1a7ed821adaee685ea2dcf423
bhayyasaheb <bhayyasahebkoke@gmail.com>
2017-12-31 14:38:33 +0530

    3)Which industry(SOC_NAME) has the most number of Data Scientist positions?

--- Hive/H1BVisaProjectHiveCommand.sql
+++ Hive/H1BVisaProjectHiveCommand.sql
@@ -7,3 +7,9 @@ hive -f Q2B_Top5LocationsInUSGotCertifiedVisaForEahYear.sql
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 
+3)Which industry(SOC_NAME) has the most number of Data Scientist positions? [certified]
+Ans:-
+
+hive -f Q3_IndustryHasMostDataScientistPositions.sql
+
+---------------------------------------------------------------------------------------------------------------------------------------------
---
+++ Hive/Q3_IndustryHasMostDataScientistPositions.sql
@@ -0,0 +1,14 @@
+
+
+USE h1b;
+
+--INSERT OVERWRITE DIRECTORY '/H1BVisaProject/Hive/Q3_IndustryHasMostDataScientistPositions' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
+SELECT soc_name, COUNT(soc_name) AS total FROM h1b_final WHERE case_status = 'CERTIFIED' AND job_title LIKE '%DATA SCIENTIST%' GROUP BY soc_name ORDER BY total DESC LIMIT 5;
+
+--soc_name	total
+--STATISTICIANS	572
+--COMPUTER AND INFORMATION RESEARCH SCIENTISTS	419
+--OPERATIONS RESEARCH ANALYSTS	380
+--Computer and Information Research Scientists	181
+--COMPUTER OCCUPATIONS, ALL OTHER	160
+
