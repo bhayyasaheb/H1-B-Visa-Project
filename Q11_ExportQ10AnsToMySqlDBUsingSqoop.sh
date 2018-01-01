443cf3b2cdff2d2b222111eaf8c6bb4f738b28a3
bhayyasaheb <bhayyasahebkoke@gmail.com>
2018-01-01 16:12:35 +0530

    Q11) Export result for question no 10 to MySql database.

---
+++ Q11_ExportQ10AnsToMySqlDBUsingSqoop.sh
@@ -0,0 +1,16 @@
+
+
+
+mysql -u root -p'password' -e 'DROP DATABASE h1b;CREATE DATABASE IF NOT EXISTS h1b;USE h1b;CREATE TABLE h1b_final(job_title VARCHAR(255) NOT NULL,petitions INT NOT NULL,success_rate FLOAT NOT NULL);';
+
+
+
+#sqoop list-tables --connect jdbc:mysql://localhost/h1b --username root --password root;
+
+
+sqoop export --connect jdbc:mysql://localhost/h1b --username root --password 'password' --table h1b_final --update-mode  allowinsert --export-dir /H1BVisaProject/Pig/Q10_JobPositionsWithNoPetititionsHavingSuccessRateAbove70Percent/part-r-00000 --input-fields-terminated-by '\t' -m 1 ;
+
+echo -e '\n\n Display content from MySQL Database.\n\n'
+echo -e '\n 10) Which are the  job positions along with the number of petitions which have the success rate more than 70%  in petitions (total petitions filed 1000 OR more than 1000)?\n\n'
+
+mysql -u root -p'password' -e 'SELECT * FROM h1b.h1b_final';
