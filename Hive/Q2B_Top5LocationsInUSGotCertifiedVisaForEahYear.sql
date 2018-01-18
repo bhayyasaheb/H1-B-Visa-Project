302ae413e8bd94ddbc0ef835edbf1d30c3062066
bhayyasaheb <bhayyasahebkoke@gmail.com>
2018-01-18 19:49:04 +0530

    Some Minor changes in Q2 B using Hive

--- Hive/Q2B_Top5LocationsInUSGotCertifiedVisaForEahYear.sql
+++ Hive/Q2B_Top5LocationsInUSGotCertifiedVisaForEahYear.sql
@@ -3,54 +3,66 @@
 
 USE h1b;
 
---CREATE TABLE certified2011 (year string, worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
+DROP TABLE IF EXISTS certified2011;
 
---INSERT OVERWRITE TABLE certified2011
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2011' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;	
+DROP TABLE IF EXISTS certified2012;
 
+DROP TABLE IF EXISTS certified2013;
 
---CREATE TABLE certified2012 (year string,worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
+DROP TABLE IF EXISTS certified2014;
 
---INSERT OVERWRITE TABLE certified2012
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2012' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+DROP TABLE IF EXISTS certified2015;
 
+DROP TABLE IF EXISTS certified2016;
 
---CREATE TABLE certified2013 (year string,worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
+CREATE TABLE IF NOT EXISTS certified2011 (year string, worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
 
---INSERT OVERWRITE TABLE certified2013
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2013' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+INSERT OVERWRITE TABLE certified2011
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2011' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;	
 
 
---CREATE TABLE certified2014 (year string,worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
+CREATE TABLE IF NOT EXISTS certified2012 (year string,worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
 
---INSERT OVERWRITE TABLE certified2014
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2014' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
-
-
---CREATE TABLE certified2015 (year string,worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
-
---INSERT OVERWRITE TABLE certified2015
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2015' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+INSERT OVERWRITE TABLE certified2012
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2012' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
 
 
---CREATE TABLE certified2016 (year string,worksite string, certifiedcount int)
---ROW FORMAT DELIMITED
---FIELDS TERMINATED BY '\t';
+CREATE TABLE IF NOT EXISTS certified2013 (year string,worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
 
---INSERT OVERWRITE TABLE certified2016
---SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2016' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+INSERT OVERWRITE TABLE certified2013
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2013' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
 
---INSERT OVERWRITE DIRECTORY '/H1BVisaProject/Hive/Q2B_Top5LocationsInUSGotCertifiedVisaForEachYear' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' 
+
+CREATE TABLE IF NOT EXISTS certified2014 (year string,worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
+
+INSERT OVERWRITE TABLE certified2014
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2014' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+
+
+CREATE TABLE IF NOT EXISTS certified2015 (year string,worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
+
+INSERT OVERWRITE TABLE certified2015
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2015' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+
+
+CREATE TABLE IF NOT EXISTS certified2016 (year string,worksite string, certifiedcount int)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t';
+
+INSERT OVERWRITE TABLE certified2016
+SELECT year, worksite, COUNT(case_status) AS certifiedcount FROM h1b_final WHERE year = '2016' AND case_status = 'CERTIFIED' GROUP BY year, worksite ORDER BY certifiedcount DESC LIMIT 5;
+
+INSERT OVERWRITE DIRECTORY '/H1BVisaProject/Hive/Q2B_Top5LocationsInUSGotCertifiedVisaForEachYear' ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' 
 SELECT * FROM(
 SELECT year,worksite,certifiedcount FROM certified2011
 UNION
